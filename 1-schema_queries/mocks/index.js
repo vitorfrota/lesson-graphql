69fcca453af7c2e85f244b1d6fe4323d0b1b6c3c
Vitor <vitorfrota.dev@gmail.com>
2023-07-26 08:38:07 -0400

    incluindo enum user

--- 1-schema_queries/mocks/index.js
+++ 1-schema_queries/mocks/index.js
@@ -4,14 +4,16 @@ const usersMock = [
     name: 'John Doe',
     email: 'john@doe.com',
     age: 20,
-    profile_id: 'id-1'
+    profile_id: 'id-1',
+    status: 'ACTIVE'
   },
   {
     id: 'id-2',
     name: 'Anne Doe',
     email: 'anne2023@doe.com',
     age: 31,
-    profile_id: 'id-2'
+    profile_id: 'id-2',
+    status: 'BLOCKED'
   },
 ]
 
